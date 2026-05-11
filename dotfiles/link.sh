d82b8320266b5a4c406f52713d0dcb46cfaccd8e
Ross Wang <meetuat19mar@126.com>
2026-05-12 06:14:22 +0800

    Add pre-oh-my-zsh config and shell helper utilities
    * Add dotfiles/zshrc.pre-oh-my-zsh with prompt, history, completion, key bindings
    
    * Add source_if_exists and isMac helpers to shell_common
    
    * Refactor autojump sourcing to use source_if_exists
    
    * Add symlink in link.sh for zshrc.pre-oh-my-zsh
    
    * Fix gitconfig and shell_common file modes (755 -> 644)

--- dotfiles/link.sh
+++ dotfiles/link.sh
@@ -30,4 +30,5 @@ add_link() {
 
 add_link "$SCRIPTPATH/shell_common"  "$HOME/.shell_common"
 add_link "$SCRIPTPATH/gitconfig"  "$HOME/.gitconfig"
+add_link "$SCRIPTPATH/zshrc.pre-oh-my-zsh"  "$HOME/.zshrc.pre-oh-my-zsh"
 echo "Successfully make soft link to $HOME (OK: $ok, NG: $ng)"
